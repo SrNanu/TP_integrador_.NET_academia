679faa233ab983229f0fd7db9b82c71daeb0b9dd
Juan Cruz Mondino <juancm.2000@hotmail.com>
2024-09-02 17:34:28 -0300

    ForeignKey

--- Dominio/Persona.cs
+++ Dominio/Persona.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+enum TipoPersonas
+{
+    1: "Cliente",
+    2: "Profesor"
+}
 
 namespace Dominio
 {
     public class Persona : EntidadNegocio
     {
-        //Se coloca "required adelante de cada atributo para que no salte error, se lo deja hasta que se complete el constructor."
+        //Se coloca "required" adelante de cada atributo para que no salte error, se lo deja hasta que se complete el constructor.
 
         [Key]
         public int IdPersona { get; set; }
+
+        [ForeignKey("Plan")]
+        public int IdPlan { get; set; }
 
         [Required]
         [StringLength(100)]
@@ -41,5 +51,19 @@ namespace Dominio
 
         [Required]
         public required int Legajo { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public required string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string Username { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public required string Password { get; set; }
+
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
     }
 }
