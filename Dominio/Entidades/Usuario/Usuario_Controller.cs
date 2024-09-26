3aeb2caa260ba1c88e375f069d65f63089fd9fdd
SrNanu <107449931+SrNanu@users.noreply.github.com>
2024-09-26 19:51:29 -0300

    Cambio de using a sintaxis mas nueva y facil de leer (en usuario)

--- Dominio/Entidades/Usuario/Usuario_Controller.cs
+++ Dominio/Entidades/Usuario/Usuario_Controller.cs
@@ -14,56 +14,38 @@ public class Usuario_Controller
             Console.Write("\nIngrese el tipo de usuario 1 para profesor y 0 para alumno: ");
             tipoUsuario = int.Parse(Console.ReadLine());
         } while (tipoUsuario != 0 && tipoUsuario != 1);
+
+        using var context = new AcademiaContext();
         
-        using (var context = new AcademiaContext())
-        {
-            /*
-            var usuario = new Usuario()
-            {
-                IdPlan = idPlan,
-                Nombre = nombre,
-                Apellido = apellido,
-                Edad = DateTime.Now.Year - fechaNac.Year,
-                FechaNacimiento = fechaNac,
-                Direccion = direccion,
-                Tipo = (TiposUsuarios)tipoUsuario,
-                Telefono = telefono,
-                Legajo = legajo,
-                Email = correo,
-                Username = nombreUser,
-                Password = contra,
-                Habilitado = false
-            };
-            */
-            usuario.Tipo = (TiposUsuarios)tipoUsuario;
-            context.Usuarios.Add(usuario);
-            context.SaveChanges();
-            Console.WriteLine($"Usuario creado: ID: {usuario.Id}, Nombre: {usuario.Nombre}, Apellido: {usuario.Apellido}, " +
-                          $"Edad: {usuario.Edad}, Fecha de Nacimiento: {usuario.FechaNacimiento.ToShortDateString()}, " +
-                          $"Dirección: {usuario.Direccion}, Tipo: {usuario.Tipo}, Telefono: {usuario.Telefono}, " +
-                          $"Legajo: {usuario.Legajo}, Email: {usuario.Email}, Username: {usuario.Username}, " +
-                          $"Habilitado: {usuario.Habilitado}");
+        usuario.Tipo = (TiposUsuarios)tipoUsuario;
+        context.Usuarios.Add(usuario);
+        context.SaveChanges();
+        Console.WriteLine($"Usuario creado: ID: {usuario.Id}, Nombre: {usuario.Nombre}, Apellido: {usuario.Apellido}, " +
+                        $"Edad: {usuario.Edad}, Fecha de Nacimiento: {usuario.FechaNacimiento.ToShortDateString()}, " +
+                        $"Dirección: {usuario.Direccion}, Tipo: {usuario.Tipo}, Telefono: {usuario.Telefono}, " +
+                        $"Legajo: {usuario.Legajo}, Email: {usuario.Email}, Username: {usuario.Username}, " +
+                        $"Habilitado: {usuario.Habilitado}");
         }
-    }
+    
 
     public static void LeerUsuario(int legajo)
     {
-        using (var context = new AcademiaContext())
+        using var context = new AcademiaContext();
+        
+        var usuario = context.Usuarios.FirstOrDefault(u => u.Legajo == legajo);
+        if (usuario != null)
         {
-            var usuario = context.Usuarios.FirstOrDefault(u => u.Legajo == legajo);
-            if (usuario != null)
-            {
-                Console.WriteLine($"Usuario encontrado: ID: {usuario.Id}, Nombre: {usuario.Nombre}, Apellido: {usuario.Apellido}, " +
-                          $"Edad: {usuario.Edad}, Fecha de Nacimiento: {usuario.FechaNacimiento.ToShortDateString()}, " +
-                          $"Dirección: {usuario.Direccion}, Tipo: {usuario.Tipo}, Telefono: {usuario.Telefono}, " +
-                          $"Legajo: {usuario.Legajo}, Email: {usuario.Email}, Username: {usuario.Username}, " +
-                          $"Habilitado: {usuario.Habilitado}");
-            }
-            else
-            {
-                Console.WriteLine("Usuario no encontrado.");
-            }
+            Console.WriteLine($"Usuario encontrado: ID: {usuario.Id}, Nombre: {usuario.Nombre}, Apellido: {usuario.Apellido}, " +
+                        $"Edad: {usuario.Edad}, Fecha de Nacimiento: {usuario.FechaNacimiento.ToShortDateString()}, " +
+                        $"Dirección: {usuario.Direccion}, Tipo: {usuario.Tipo}, Telefono: {usuario.Telefono}, " +
+                        $"Legajo: {usuario.Legajo}, Email: {usuario.Email}, Username: {usuario.Username}, " +
+                        $"Habilitado: {usuario.Habilitado}");
         }
+        else
+        {
+            Console.WriteLine("Usuario no encontrado.");
+        }
+        
     }
 
     public static void ActualizarUsuario(int legajo)
@@ -89,100 +71,100 @@ public class Usuario_Controller
 
         if (decision != 0)
         {
-            using (var context = new AcademiaContext())
-            {
-                var usuario = context.Usuarios.FirstOrDefault(u => u.Legajo == legajo);
-                if (usuario != null)
-                {
-                    switch (decision)
-                    {
-                        case 1:
-                            Console.Write("\nIngrese el nuevo nombre: ");
-                            usuario.Nombre = Console.ReadLine();
-                            break;
-
-                        case 2:
-                            Console.Write("\nIngrese el nuevo apellido: ");
-                            usuario.Apellido = Console.ReadLine();
-                            break;
-
-                        case 3:
-                            Console.Write("\nIngrese la nueva fecha de nacimiento (formato: dd/MM/yyyy): ");
-                            string fechaInput = Console.ReadLine();
-                            if (DateTime.TryParseExact(fechaInput, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime nuevaFechaNacimiento))
-                            {
-                                usuario.FechaNacimiento = nuevaFechaNacimiento;
-                            }
-                            else
-                            {
-                                Console.WriteLine("El formato de fecha no es válido. Intente nuevamente.");
-                            }
-                            break;
-
-                        case 4:
-                            Console.Write("\nIngrese la nueva dirección: ");
-                            usuario.Direccion = Console.ReadLine();
-                            break;
-
-                        case 5:
-                            Console.Write("\nIngrese el nuevo teléfono: ");
-                            usuario.Telefono = Console.ReadLine();
-                            break;
-
-                        case 6:
-                            Console.Write("\nIngrese la nueva legajo: ");
-                            usuario.Legajo = int.Parse(Console.ReadLine());
-                            break;
-
-                        case 7:
-                            Console.Write("\nIngrese el nuevo correo: ");
-                            usuario.Email = Console.ReadLine();
-                            break;
-
-                        case 8:
-                            Console.Write("\nIngrese el nuevo nombre de usuario: ");
-                            usuario.Username = Console.ReadLine();
-                            break;
-
-                        case 9:
-                            Console.Write("\nIngrese la nueva contraseña: ");
-                            usuario.Password = Console.ReadLine();
-                            break;
-                    }
-                    context.SaveChanges();
-                    Console.WriteLine($"Usuario actualizado: ID: {usuario.Id}, Nombre: {usuario.Nombre}, Apellido: {usuario.Apellido}, " +
-                          $"Edad: {usuario.Edad}, Fecha de Nacimiento: {usuario.FechaNacimiento.ToShortDateString()}, " +
-                          $"Dirección: {usuario.Direccion}, Tipo: {usuario.Tipo}, Telefono: {usuario.Telefono}, " +
-                          $"Legajo: {usuario.Legajo}, Email: {usuario.Email}, Username: {usuario.Username}, " +
-                          $"Habilitado: {usuario.Habilitado}");
-                }
-                else
-                {
-                    Console.WriteLine("Usuario no encontrado.");
-                }
-            }
-        }
-    }
-
-    public static void EliminarUsuario(int legajo)
-    {
-        using (var context = new AcademiaContext())
-        {
+            using var context = new AcademiaContext();
+            
             var usuario = context.Usuarios.FirstOrDefault(u => u.Legajo == legajo);
             if (usuario != null)
             {
-                context.Usuarios.Remove(usuario);
+                switch (decision)
+                {
+                    case 1:
+                        Console.Write("\nIngrese el nuevo nombre: ");
+                        usuario.Nombre = Console.ReadLine();
+                        break;
+
+                    case 2:
+                        Console.Write("\nIngrese el nuevo apellido: ");
+                        usuario.Apellido = Console.ReadLine();
+                        break;
+
+                    case 3:
+                        Console.Write("\nIngrese la nueva fecha de nacimiento (formato: dd/MM/yyyy): ");
+                        string fechaInput = Console.ReadLine();
+                        if (DateTime.TryParseExact(fechaInput, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime nuevaFechaNacimiento))
+                        {
+                            usuario.FechaNacimiento = nuevaFechaNacimiento;
+                        }
+                        else
+                        {
+                            Console.WriteLine("El formato de fecha no es válido. Intente nuevamente.");
+                        }
+                        break;
+
+                    case 4:
+                        Console.Write("\nIngrese la nueva dirección: ");
+                        usuario.Direccion = Console.ReadLine();
+                        break;
+
+                    case 5:
+                        Console.Write("\nIngrese el nuevo teléfono: ");
+                        usuario.Telefono = Console.ReadLine();
+                        break;
+
+                    case 6:
+                        Console.Write("\nIngrese la nueva legajo: ");
+                        usuario.Legajo = int.Parse(Console.ReadLine());
+                        break;
+
+                    case 7:
+                        Console.Write("\nIngrese el nuevo correo: ");
+                        usuario.Email = Console.ReadLine();
+                        break;
+
+                    case 8:
+                        Console.Write("\nIngrese el nuevo nombre de usuario: ");
+                        usuario.Username = Console.ReadLine();
+                        break;
+
+                    case 9:
+                        Console.Write("\nIngrese la nueva contraseña: ");
+                        usuario.Password = Console.ReadLine();
+                        break;
+                }
                 context.SaveChanges();
-                Console.WriteLine($"Usuario eliminado: ID: {usuario.Id}, Nombre: {usuario.Nombre}, Apellido: {usuario.Apellido}, " +
-                          $"Edad: {usuario.Edad}, Fecha de Nacimiento: {usuario.FechaNacimiento.ToShortDateString()}, " +
-                          $"Dirección: {usuario.Direccion}, Tipo: {usuario.Tipo}, Telefono: {usuario.Telefono}, " +
-                          $"Legajo: {usuario.Legajo}, Email: {usuario.Email}, Username: {usuario.Username}, " +
-                          $"Habilitado: {usuario.Habilitado}");
+                Console.WriteLine($"Usuario actualizado: ID: {usuario.Id}, Nombre: {usuario.Nombre}, Apellido: {usuario.Apellido}, " +
+                        $"Edad: {usuario.Edad}, Fecha de Nacimiento: {usuario.FechaNacimiento.ToShortDateString()}, " +
+                        $"Dirección: {usuario.Direccion}, Tipo: {usuario.Tipo}, Telefono: {usuario.Telefono}, " +
+                        $"Legajo: {usuario.Legajo}, Email: {usuario.Email}, Username: {usuario.Username}, " +
+                        $"Habilitado: {usuario.Habilitado}");
             }
             else
             {
                 Console.WriteLine("Usuario no encontrado.");
             }
+            
         }
+    }
+
+    public static void EliminarUsuario(int legajo)
+    {
+        using var context = new AcademiaContext();
+        
+        var usuario = context.Usuarios.FirstOrDefault(u => u.Legajo == legajo);
+        if (usuario != null)
+        {
+            context.Usuarios.Remove(usuario);
+            context.SaveChanges();
+            Console.WriteLine($"Usuario eliminado: ID: {usuario.Id}, Nombre: {usuario.Nombre}, Apellido: {usuario.Apellido}, " +
+                        $"Edad: {usuario.Edad}, Fecha de Nacimiento: {usuario.FechaNacimiento.ToShortDateString()}, " +
+                        $"Dirección: {usuario.Direccion}, Tipo: {usuario.Tipo}, Telefono: {usuario.Telefono}, " +
+                        $"Legajo: {usuario.Legajo}, Email: {usuario.Email}, Username: {usuario.Username}, " +
+                        $"Habilitado: {usuario.Habilitado}");
+        }
+        else
+        {
+            Console.WriteLine("Usuario no encontrado.");
+        }
+        
     }
 }
--- Dominio/Shared/AcademiaContext.cs
+++ Dominio/Shared/AcademiaContext.cs
@@ -26,7 +26,6 @@ public class AcademiaContext : DbContext
     {
         optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Initial Catalog=AcademiaDb");
     }
-
     //Si lo queremos en MySQL
     /*
      optionsBuilder.UseMySql("Server=localhost;Database=Academia;User=root;Password=root;",
