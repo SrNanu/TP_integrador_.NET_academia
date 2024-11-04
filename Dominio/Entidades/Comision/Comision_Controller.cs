54ad959e94d4a3c047c2a48535ce910a3505de98
SrNanu <107449931+SrNanu@users.noreply.github.com>
2024-11-04 17:06:55 -0300

    comision controller hecho

--- Dominio/Entidades/Comision/Comision_Controller.cs
+++ Dominio/Entidades/Comision/Comision_Controller.cs
@@ -7,111 +7,57 @@ using System.Threading.Tasks;
 
 public class Comision_Controller
 {
-    public static void CrearComision(int idPlan, string descripcion, int anioEspecialidad)
+    public static void CrearComision(Comision comision)
     {
-        using (var context = new AcademiaContext())
-        {
-            var comision = new Comision()
-            {
-                IdPlan = idPlan,
-                Descripcion = descripcion,
-                AnioEspecialidad = anioEspecialidad
-            };
 
-            context.Comisiones.Add(comision);
+        using var context = new AcademiaContext();
+
+        context.Comisiones.Add(comision);
+        context.SaveChanges();
+
+    }
+
+    public static Comision? GetOneComision(int id)
+    {
+        using var context = new AcademiaContext();
+
+        return context.Comisiones.Find(id);
+
+    }
+
+    public static IEnumerable<Comision> GetAllComision()
+    {
+        using var context = new AcademiaContext();
+
+        return context.Comisiones.ToList();
+    }
+
+    public static void ActualizarComision(Comision comision)
+    {
+        using var context = new AcademiaContext();
+
+        var comisionToUpdate = context.Comisiones.Find(comision.Id);
+
+        if (comisionToUpdate != null)
+        {
+            comisionToUpdate.Descripcion = comision.Descripcion;
+            comisionToUpdate.IdPlan = comision.IdPlan;
+            comisionToUpdate.AnioEspecialidad = comision.AnioEspecialidad;
+            comisionToUpdate.Id = comision.Id;
             context.SaveChanges();
-            Console.WriteLine($"Comisión creada: ID: {comision.Id}, ID Plan: {comision.IdPlan}, " +
-                              $"Descripción: {comision.Descripcion}, Año Especialidad: {comision.AnioEspecialidad}");
-        }
-    }
-
-    public static void LeerComision(int id)
-    {
-        using (var context = new AcademiaContext())
-        {
-            var comision = context.Comisiones.FirstOrDefault(c => c.Id == id);
-            if (comision != null)
-            {
-                Console.WriteLine($"Comisión encontrada: ID: {comision.Id}, ID Plan: {comision.IdPlan}, " +
-                                  $"Descripción: {comision.Descripcion}, Año Especialidad: {comision.AnioEspecialidad}");
-
-            }
-            else
-            {
-                Console.WriteLine("Comision no encontrado.");
-            }
-        }
-    }
-
-    public static void ActualizarComision(int id)
-    {
-        int decision = 10;
-
-        while (decision > 3 || decision < 0)
-        {
-            Console.WriteLine("\n¿Qué propiedad desea modificar?" +
-            "              \n   1- Id del plan" +
-            "              \n   2- Descripción" +
-            "              \n   3- Año de especialidad" +
-            "              \n   0- Salir");
-            Console.Write("\nIngrese su decisión: ");
-            decision = int.Parse(Console.ReadLine());
-        }
-
-        if (decision != 0)
-        {
-            using (var context = new AcademiaContext())
-            {
-                var comision = context.Comisiones.FirstOrDefault(c => c.Id == id);
-                if (comision != null)
-                {
-                    switch (decision)
-                    {
-                        case 1:
-                            Console.Write("\nIngrese el nuevo id del plan: ");
-                            comision.IdPlan = int.Parse(Console.ReadLine());
-                            break;
-
-                        case 2:
-                            Console.Write("\nIngrese la nueva descripción: ");
-                            comision.Descripcion = Console.ReadLine();
-                            break;
-
-                        case 3:
-                            Console.Write("\nIngrese el nuevo año de especialidad: ");
-                            comision.AnioEspecialidad = int.Parse(Console.ReadLine());
-                            break;
-                    }
-                    context.SaveChanges();
-                    Console.WriteLine($"Comisión actualizada: ID: {comision.Id}, ID Plan: {comision.IdPlan}, " +
-                                      $"Descripción: {comision.Descripcion}, Año Especialidad: {comision.AnioEspecialidad}");
-
-                }
-                else
-                {
-                    Console.WriteLine("Comision no encontrado.");
-                }
-            }
         }
     }
 
     public static void EliminarComision(int id)
     {
-        using (var context = new AcademiaContext())
-        {
-            var comision = context.Comisiones.FirstOrDefault(c => c.Id == id);
-            if (comision != null)
-            {
-                context.Comisiones.Remove(comision);
-                context.SaveChanges();
-                Console.WriteLine($"Comisión eliminada: ID: {comision.Id}, ID Plan: {comision.IdPlan}, " +
-                                  $"Descripción: {comision.Descripcion}, Año Especialidad: {comision.AnioEspecialidad}");
+        using var context = new AcademiaContext();
 
-            }
-            else
-            {
-                Console.WriteLine("Comision no encontrado.");
-            }
+        var comision = context.Comisiones.Find(id);
+        if (comision != null)
+        {
+            context.Comisiones.Remove(comision);
+            context.SaveChanges();
         }
+
     }
 }
