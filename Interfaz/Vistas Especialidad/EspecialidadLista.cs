168879ffa9995cfc5647c530de61e6560b72fd8f
SrNanu <107449931+SrNanu@users.noreply.github.com>
2024-11-05 12:22:45 -0300

    vistas de especialidad hechas

---
+++ Interfaz/Vistas Especialidad/EspecialidadLista.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz.Vistas_Especialidad
+{
+    public partial class EspecialidadLista : Form
+    {
+        public EspecialidadLista()
+        {
+            InitializeComponent();
+        }
+    }
+}
