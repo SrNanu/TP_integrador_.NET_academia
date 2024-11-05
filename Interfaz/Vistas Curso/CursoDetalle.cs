2fada0b4922bfd899a20e7ff94e1d4b8880c15d4
SrNanu <107449931+SrNanu@users.noreply.github.com>
2024-11-05 12:34:55 -0300

    vistas curso hechas

---
+++ Interfaz/Vistas Curso/CursoDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz.Vistas_Cursos
+{
+    public partial class CursoDetalle : Form
+    {
+        public CursoDetalle()
+        {
+            InitializeComponent();
+        }
+    }
+}
