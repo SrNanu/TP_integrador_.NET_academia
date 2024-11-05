42d277a7266165161b16350ce4068f65315a8dbd
Juan Cruz Mondino <juancm.2000@hotmail.com>
2024-11-05 09:34:33 -0300

    moidifcoConnectionString

--- Dominio/Shared/AcademiaContext.cs
+++ Dominio/Shared/AcademiaContext.cs
@@ -24,7 +24,7 @@ public class AcademiaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Initial Catalog=AcademiaDb");
+        optionsBuilder.UseSqlServer("Data Source=DESKTOP-6HBL1R6\SQLEXPRESS;Initial Catalog=Academia;Integrated Security=True;Encrypt=False");
     }
     //Si lo queremos en MySQL
     /*
