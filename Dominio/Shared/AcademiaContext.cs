8a241254a36e2ea856954dc7e7f0e9a6e93ecc12
Juan Cruz Mondino <juancm.2000@hotmail.com>
2024-11-11 19:58:34 -0300

    puliendoUltimoMomento

--- Dominio/Shared/AcademiaContext.cs
+++ Dominio/Shared/AcademiaContext.cs
@@ -18,15 +18,16 @@ public class AcademiaContext : DbContext
     public DbSet<AlumnoInscripcion> AlumnoInscripciones { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-
     {
 
         //optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Initial Catalog=AcademiaDb"); //SANTINO
 
         //optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-6HBL1R6\SQLEXPRESS;Initial Catalog=Academia;Integrated Security=True;Encrypt=False"); // JUAN CRUZ - Notebook
-        optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-KD3CGEG\SQLEXPRESS;Initial Catalog=Academia;Integrated Security=True;Encrypt=False"); // JUAN CRUZ - Pc
+        //optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-KD3CGEG\SQLEXPRESS;Initial Catalog=Academia;Integrated Security=True;Encrypt=False"); // JUAN CRUZ - Pc
 
         //optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-6HBL1R6\SQLEXPRESS;Initial Catalog=Academia;Integrated Security=True;Encrypt=False"); //LUCIO
+        
+        optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Initial Catalog=Academia;Integrated Security=True;Encrypt=False"); //universal
 
 
     }
